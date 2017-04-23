04c9c7334d7e274940c0cfaa2e9fb032e1431e70
Ibis Prevedello <ibiscp@gmail.com>
2017-04-23 02:15:44 -0300

    feat: Add KF

--- Runaway Robot/studentMain.py
+++ Runaway Robot/studentMain.py
@@ -1,50 +1,19 @@
-__author__ = 'Ibis'
-
 # ----------
-# Background
+# Part Two
 #
-# A robotics company named Trax has created a line of small self-driving robots
-# designed to autonomously traverse desert environments in search of undiscovered
-# water deposits.
-#
-# A Traxbot looks like a small tank. Each one is about half a meter long and drives
-# on two continuous metal tracks. In order to maneuver itself, a Traxbot can do one
-# of two things: it can drive in a straight line or it can turn. So to make a
-# right turn, A Traxbot will drive forward, stop, turn 90 degrees, then continue
-# driving straight.
-#
-# This series of questions involves the recovery of a rogue Traxbot. This bot has
-# gotten lost somewhere in the desert and is now stuck driving in an almost-circle: it has
-# been repeatedly driving forward by some step size, stopping, turning a certain
-# amount, and repeating this process... Luckily, the Traxbot is still sending all
-# of its sensor data back to headquarters.
-#
-# In this project, we will start with a simple version of this problem and
-# gradually add complexity. By the end, you will have a fully articulated
-# plan for recovering the lost Traxbot.
-#
-# ----------
-# Part One
-#
-# Let's start by thinking about circular motion (well, really it's polygon motion
-# that is close to circular motion). Assume that Traxbot lives on
-# an (x, y) coordinate plane and (for now) is sending you PERFECTLY ACCURATE sensor
-# measurements.
-#
-# With a few measurements you should be able to figure out the step size and the
-# turning angle that Traxbot is moving with.
-# With these two pieces of information, you should be able to
-# write a function that can predict Traxbot's next location.
-#
-# You can use the robot class that is already written to make your life easier.
-# You should re-familiarize yourself with this class, since some of the details
-# have changed.
+# Now we'll make the scenario a bit more realistic. Now Traxbot's
+# sensor measurements are a bit noisy (though its motions are still
+# completetly noise-free and it still moves in an almost-circle).
+# You'll have to write a function that takes as input the next
+# noisy (x, y) sensor measurement and outputs the best guess
+# for the robot's next position.
 #
 # ----------
 # YOUR JOB
 #
-# Complete the estimate_next_pos function. You will probably want to use
-# the OTHER variable to keep track of information about the runaway robot.
+# Complete the function estimate_next_pos. You will be considered
+# correct if your estimate is within 0.01 stepsizes of Traxbot's next
+# true position.
 #
 # ----------
 # GRADING
@@ -57,90 +26,54 @@ __author__ = 'Ibis'
 
 # These import steps give you access to libraries which you may (or may
 # not) want to use.
-from robot import *
+from robot import *  # Check the robot.py tab to see how this works.
 from math import *
-from matrix import *
+from matrix import * # Check the matrix.py tab to see how this works.
 import random
 
-
-# This is the function you have to write. The argument 'measurement' is a
+# This is the function you have to write. Note that measurement is a
 # single (x, y) point. This function will have to be called multiple
 # times before you have enough information to accurately predict the
 # next position. The OTHER variable that your function returns will be
 # passed back to your function the next time it is called. You can use
 # this to keep track of important information over time.
-
-xInicial = 2.1
-yInicial = 4.3
-# dt = 0.1
-#
-# x = matrix([[0.], [0.], [0.], [0.], [0.]]) # initial state (location and velocity)
-# u = matrix([[0.], [0.], [0.], [0.], [0.]]) # external motion
-# P = matrix([[1000., 0., 0., 0., 0.], [0., 1000., 0., 0., 0.], [0., 0., 1000., 0., 0.], [0., 0., 0., 1000., 0.], [0., 0., 0., 0., 1000.]]) # initial uncertainty: 0 for positions x and y, 1000 for the two velocities
-# #F = matrix([[1., 0., dt, 0., 0., 0.], [0., 1., 0., dt, 0., 0.], [0., 0., 1., 0., dt, 0.], [0., 0., 0., 1., 0., dt],[0., 0., 0., 0., 1., 0.],[0., 0., 0., 0., 0., 1.]]) # next state function: generalize the 2d version to 4d
-# H = matrix([[1., 0., 0., 0., 0.], [0., 1., 0., 0., 0.]]) # measurement function: reflect the fact that we observe x and y but not the two velocities
-# R = matrix([[.1, 0.], [0, .1]]) # measurement uncertainty: use 2x2 matrix with 0.1 as main diagonal
-# I = matrix([[1., 0., 0., 0., 0.], [0., 1., 0., 0., 0.], [0., 0., 1., 0., 0.], [0., 0., 0., 1., 0.], [0., 0., 0., 0., 1.]]) # 4d identity matrix
-#
-# # F matrix
-# def transitionMatrix(state):
-#         h = state.value[2][0] # heading
-#         r = state.value[3][0]  # turning
-#         d = state.value[4][0] # distance
-#         return matrix([[1., 0., -d*sin(h+r), -d*sin(h+r), cos(h+r)],
-#                        [0., 1.,  d*cos(h+r),  d*cos(h+r), sin(h+r)],
-#                        [0., 0.,          1.,          1.,       0.],
-#                        [0., 0.,          0.,          1.,       0.],
-#                        [0., 0.,          0.,          0.,       1.]])
-
 def estimate_next_pos(measurement, OTHER = None):
-    # global x, P
+    global x, u, P, F, H, R
     """Estimate the next (x, y) position of the wandering Traxbot
     based on noisy (x, y) measurements."""
+    dt = 0.1
+    dt_2 = dt * dt
+    dt_3 = dt_2 * dt
+    dt_4 = dt_3 * dt
 
-    # # prediction
-    # F = transitionMatrix(x)
-    # x = (F * x) + u
-    # P = F * P * F.transpose()
-    #
-    # # measurement update
-    # Z = matrix([[measurement[0],measurement[1]]])
-    # y = Z.transpose() - (H * x)
-    # S = H * P * H.transpose() + R
-    # K = P * H.transpose() * S.inverse()
-    # x = x + (K * y)
-    # P = (I - (K * H)) * P
+    Q = matrix([[dt_4 / 4 * measurement_noise, 0., dt_3 / 2 * measurement_noise, 0.],
+                [0., dt_4 / 4 * measurement_noise, 0., dt_3 / 2 * measurement_noise],
+                [dt_3 / 2 * measurement_noise, 0., dt_2 * measurement_noise, 0.],
+                [0., dt_3 / 2 * measurement_noise, 0., dt_2 * measurement_noise],])
 
-    """Estimate the next (x, y) position of the wandering Traxbot
-    based on noisy (x, y) measurements."""
+    ## Predict
+    x = F * x + u
+    P = F * P * F.transpose() + Q
 
-    if not OTHER:
-        opoint = (0.0, 0.0)
-        theta = 0.0
-    else:
-        opoint = OTHER[0]
-        theta = OTHER[1]
-    cpoint = measurement
-    delta_y = cpoint[1] - opoint[1]
-    delta_x = cpoint[0] - opoint[0]
+    ## Update
+    z_pred = H * x
+    z = matrix([[measurement[0]], [measurement[1]]])
+    y = z - z_pred
+    Ht = H.transpose()
+    S = H * P * Ht + R
+    PHt = P * Ht
+    K = PHt * S.inverse()
 
-    beta = atan2(delta_y, delta_x)
-    db = distance_between(opoint, cpoint)
-
-    turn_angle = beta - theta
-    print "turn_angle: ", turn_angle
-
-    heading = beta + turn_angle
-    x_prime = cpoint[0] + db * cos(heading)
-    y_prime = cpoint[1] + db * sin(heading)
-
-    xy_estimate = (x_prime, y_prime)
-    OTHER = [cpoint, beta]
+    ## New estimate
+    x = x + (K * y)
+    I = matrix([[]])
+    I.identity(4)
+    P = (I - K * H) * P
 
     # You must return xy_estimate (x, y), and OTHER (even if it is None)
     # in this order for grading purposes.
-    # xy_estimate = [x.value[0][0],x.value[1][0]]
-    return xy_estimate, OTHER
+    xy_estimate = (x.value[0][0], x.value[1][0])
+    return xy_estimate
 
 # A helper function you may find useful.
 def distance_between(point1, point2):
@@ -159,7 +92,31 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
     # if you haven't localized the target bot, make a guess about the next
     # position, then we move the bot and compare your guess to the true
     # next position. When you are close enough, we stop checking.
-    while not localized and ctr <= 10:
+    #For Visualization
+    import turtle    #You need to run this locally to use the turtle module
+    window = turtle.Screen()
+    window.bgcolor('white')
+    size_multiplier= 25.0  #change Size of animation
+    broken_robot = turtle.Turtle()
+    broken_robot.shape('turtle')
+    broken_robot.color('green')
+    broken_robot.resizemode('user')
+    broken_robot.shapesize(0.1, 0.1, 0.1)
+    measured_broken_robot = turtle.Turtle()
+    measured_broken_robot.shape('circle')
+    measured_broken_robot.color('red')
+    measured_broken_robot.resizemode('user')
+    measured_broken_robot.shapesize(0.1, 0.1, 0.1)
+    prediction = turtle.Turtle()
+    prediction.shape('arrow')
+    prediction.color('blue')
+    prediction.resizemode('user')
+    prediction.shapesize(0.1, 0.1, 0.1)
+    prediction.penup()
+    broken_robot.penup()
+    measured_broken_robot.penup()
+    #End of Visualization
+    while not localized and ctr <= 1000:
         ctr += 1
         measurement = target_bot.sense()
         position_guess, OTHER = estimate_next_pos_fcn(measurement, OTHER)
@@ -171,6 +128,17 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
             localized = True
         if ctr == 10:
             print "Sorry, it took you too many steps to localize the target."
+        #More Visualization
+        measured_broken_robot.setheading(target_bot.heading*180/pi)
+        measured_broken_robot.goto(measurement[0]*size_multiplier, measurement[1]*size_multiplier-200)
+        measured_broken_robot.stamp()
+        broken_robot.setheading(target_bot.heading*180/pi)
+        broken_robot.goto(target_bot.x*size_multiplier, target_bot.y*size_multiplier-200)
+        broken_robot.stamp()
+        prediction.setheading(target_bot.heading*180/pi)
+        prediction.goto(position_guess[0]*size_multiplier, position_guess[1]*size_multiplier-200)
+        prediction.stamp()
+        #End of Visualization
     return localized
 
 # def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
@@ -180,30 +148,6 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
 #     # if you haven't localized the target bot, make a guess about the next
 #     # position, then we move the bot and compare your guess to the true
 #     # next position. When you are close enough, we stop checking.
-#     #For Visualization
-#     import turtle    #You need to run this locally to use the turtle module
-#     window = turtle.Screen()
-#     window.bgcolor('white')
-#     size_multiplier= 25.0  #change Size of animation
-#     broken_robot = turtle.Turtle()
-#     broken_robot.shape('turtle')
-#     broken_robot.color('green')
-#     broken_robot.resizemode('user')
-#     broken_robot.shapesize(0.1, 0.1, 0.1)
-#     measured_broken_robot = turtle.Turtle()
-#     measured_broken_robot.shape('circle')
-#     measured_broken_robot.color('red')
-#     measured_broken_robot.resizemode('user')
-#     measured_broken_robot.shapesize(0.1, 0.1, 0.1)
-#     prediction = turtle.Turtle()
-#     prediction.shape('arrow')
-#     prediction.color('blue')
-#     prediction.resizemode('user')
-#     prediction.shapesize(0.1, 0.1, 0.1)
-#     prediction.penup()
-#     broken_robot.penup()
-#     measured_broken_robot.penup()
-#     #End of Visualization
 #     while not localized and ctr <= 1000:
 #         ctr += 1
 #         measurement = target_bot.sense()
@@ -214,34 +158,38 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
 #         if error <= distance_tolerance:
 #             print "You got it right! It took you ", ctr, " steps to localize."
 #             localized = True
-#         if ctr == 10:
+#         if ctr == 1000:
 #             print "Sorry, it took you too many steps to localize the target."
-#         #More Visualization
-#         measured_broken_robot.setheading(target_bot.heading*180/pi)
-#         measured_broken_robot.goto(measurement[0]*size_multiplier, measurement[1]*size_multiplier-200)
-#         measured_broken_robot.stamp()
-#         broken_robot.setheading(target_bot.heading*180/pi)
-#         broken_robot.goto(target_bot.x*size_multiplier, target_bot.y*size_multiplier-200)
-#         broken_robot.stamp()
-#         prediction.setheading(target_bot.heading*180/pi)
-#         prediction.goto(position_guess[0]*size_multiplier, position_guess[1]*size_multiplier-200)
-#         prediction.stamp()
-#         #End of Visualization
 #     return localized
 
 # This is a demo for what a strategy could look like. This one isn't very good.
 def naive_next_pos(measurement, OTHER = None):
+    global x
     """This strategy records the first reported position of the target and
     assumes that eventually the target bot will eventually return to that
     position, so it always guesses that the first position will be the next."""
     if not OTHER: # this is the first measurement
         OTHER = measurement
-    xy_estimate = OTHER
+        xy_estimate = OTHER
+        x = matrix([[measurement[0]], [measurement[1]], [0.], [0.]])  # initial state (location and velocity)
+    else:
+        xy_estimate = estimate_next_pos(measurement)
     return xy_estimate, OTHER
 
 # This is how we create a target bot. Check the robot.py file to understand
 # How the robot class behaves.
-test_target = robot(xInicial, yInicial, 0.5, 2*pi / 34.0, 1.5)
-test_target.set_noise(0.0, 0.0, 0.0)
+test_target = robot(2.1, 4.3, 0.5, 2*pi / 34.0, 1.5)
+measurement_noise = 0.05 * test_target.distance
+test_target.set_noise(0.0, 0.0, measurement_noise)
 
-demo_grading(estimate_next_pos, test_target)
+x = matrix([[0.], [0.], [0.], [0.]])  # initial state (location and velocity)
+u = matrix([[0.], [0.], [0.], [0.]]) # external motion
+P = matrix([[1000., 0., 0., 0.],
+            [0., 1000., 0., 0.],
+            [0., 0., 1000., 0.],
+            [0., 0., 0., 1000.]]) # initial uncertainty
+F = matrix([[1., 0., 0.1, 0.], [0., 1., 0., 0.1], [0., 0., 1., 0.], [0., 0., 0., 1.],]) # next state function
+H = matrix([[1., 0., 0., 0.], [0., 1., 0., 0.]]) # measurement function
+R = matrix([[1., 0.], [0., 1.]]) # measurement uncertainty
+
+demo_grading(naive_next_pos, test_target)
